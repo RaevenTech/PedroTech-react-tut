9c746bca27910c2c3e4c6b8e037c3c27fad533a8
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-08-15 15:16:31 +0100

    using props

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Jobs from "./components/Jobs";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            {" "}
+            <Jobs salary={2000} position="FE" company="Microsoft" />
+            <Jobs salary={2500} position="BE" company="Apple" />
+            <Jobs salary={3000} position="FS" company="Google" />
+        </div>
+    );
 }
 
 export default App;
---
+++ practice/src/components/Jobs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jobs = (props) => {
+    return (
+        <div>
+            <h1>{props.salary}</h1>
+            <h2>{props.position}</h2>
+            <h3>{props.company}</h3>
+        </div>
+    );
+};
+
+export default Jobs;
