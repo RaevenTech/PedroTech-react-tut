572b823592dce8e300a3dc8a2c41c3037ad356ff
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-08-18 15:53:37 +0100

    del not work

--- practice/src/components/CRUD/ToDo.js
+++ practice/src/components/CRUD/ToDo.js
@@ -1,9 +1,37 @@
-import React from "react";
+import { useState } from "react";
 
 const ToDo = () => {
+    const [todoList, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    const handleChange = (e) => {
+        setNewTask(e.target.value);
+    };
+
+    const addTask = () => {
+        setTodoList([...todoList, newTask]);
+    };
+
+    const deleteTask = (i) => {
+        setTodoList([todoList.find((task) => task !== i)]);
+    };
     return (
         <>
             <div>CrudToDo</div>
+            <div className="addTask">
+                <input onChange={handleChange} />
+                <button onClick={addTask}>Add Task</button>
+            </div>
+            <div className="list">
+                {todoList.map((task, i) => (
+                    <div key={i}>
+                        <h3>{task}</h3>
+                        <span>
+                            <button onClick={() => deleteTask(i)}>X</button>
+                        </span>
+                    </div>
+                ))}
+            </div>
         </>
     );
 };
